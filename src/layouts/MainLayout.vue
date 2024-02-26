1d89c84631310f1360c71c07b20c0ecace739f4a
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-02-26 18:21:14 +0100

    Add Blue Rays Regular font and update app layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,41 +2,25 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-        <q-toolbar-title> CineShop </q-toolbar-title>
+        <q-toolbar-title>
+          <q-btn flat dense icon="local_movies" to="/"></q-btn>
+          CINESHOP
+        </q-toolbar-title>
+
+        <q-tabs align="left" class="q-ma-lg">
+          <q-tab to="/" label="Accueil" exact />
+          <q-tab to="/dvd" label="DVD" />
+          <q-tab to="/blu-ray" label="Blu-Ray" />
+          <q-tab to="/box-sets" label="Coffrets" />
+          <q-tab to="/deals" label="Bons Plans" />
+        </q-tabs>
+
         <div style="flex-grow: 1"></div>
+
         <q-btn icon="shopping_cart" round @click="goToCart" />
         <q-btn icon="person" round @click="goToAccount" />
       </q-toolbar>
     </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header>Navigation</q-item-label>
-        <q-item to="/" exact>
-          <q-item-section>Accueil</q-item-section>
-        </q-item>
-        <q-item to="/dvd">
-          <q-item-section>DVD</q-item-section>
-        </q-item>
-        <q-item to="/blu-ray">
-          <q-item-section>Blu-Ray</q-item-section>
-        </q-item>
-        <q-item to="/box-sets">
-          <q-item-section>Coffrets</q-item-section>
-        </q-item>
-        <q-item to="/deals">
-          <q-item-section>Bons Plans</q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -44,43 +28,41 @@
 
     <q-footer>
       <q-toolbar>
-        <q-toolbar-title> Nom du Magasin &copy; 2024 </q-toolbar-title>
+        <q-toolbar-title> CINESHOP &copy; 2024 </q-toolbar-title>
       </q-toolbar>
     </q-footer>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
 import { useRouter } from "vue-router";
 
 export default defineComponent({
   name: "MainLayout",
 
   setup() {
-    const leftDrawerOpen = ref(false);
     const router = useRouter();
 
-    function toggleLeftDrawer() {
-      leftDrawerOpen.value = !leftDrawerOpen.value;
-    }
-
     function goToCart() {
-      // Logique pour aller à la page du panier
       router.push("/cart");
     }
 
     function goToAccount() {
-      // Logique pour aller à la page du compte utilisateur
       router.push("/account");
     }
 
     return {
-      leftDrawerOpen,
-      toggleLeftDrawer,
       goToCart,
       goToAccount,
     };
   },
 });
 </script>
+
+<style>
+body,
+.q-layout {
+  font-family: "Blue Rays Regular", sans-serif;
+}
+</style>
