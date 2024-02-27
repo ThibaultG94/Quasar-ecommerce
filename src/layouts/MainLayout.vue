a633478c99058c8170b1d7a5a22b7cef8196dff1
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-02-27 17:46:45 +0100

    Update font file path and style in fonts.scss and MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,16 +8,16 @@
         </q-toolbar-title>
 
         <q-tabs align="left" class="q-ma-lg">
-          <q-tab to="/" label="Accueil" exact />
-          <q-tab to="/dvd" label="DVD" />
-          <q-tab to="/blu-ray" label="Blu-Ray" />
-          <q-tab to="/box-sets" label="Coffrets" />
-          <q-tab to="/deals" label="Bons Plans" />
+          <q-route-tab to="/" label="Accueil" exact />
+          <q-route-tab to="/dvd" label="DVD" exact />
+          <q-route-tab to="/blu-ray" label="Blu-Ray" exact />
+          <q-route-tab to="/box-sets" label="Coffrets" exact />
+          <q-route-tab to="/deals" label="Bons Plans" exact />
         </q-tabs>
 
         <div style="flex-grow: 1"></div>
 
-        <q-btn icon="shopping_cart" round @click="goToCart" />
+        <q-btn icon="shopping_cart" round @click="goToCart" class="q-mr-sm" />
         <q-btn icon="person" round @click="goToAccount" />
       </q-toolbar>
     </q-header>
@@ -64,5 +64,31 @@ export default defineComponent({
 body,
 .q-layout {
   font-family: "Blue Rays Regular", sans-serif;
+}
+.q-tab__label {
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 1rem;
+  font-weight: bold;
+}
+.q-tab {
+  position: relative;
+  overflow: hidden;
+}
+.q-tab::after {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%) scaleX(0);
+  transform-origin: center bottom;
+  width: 100%;
+  height: 2px;
+  background-color: #ffffff;
+  transition: transform 0.3s ease-in-out;
+}
+
+.q-tab:hover::after,
+.q-tabs .q-tab--active::after {
+  transform: translateX(-50%) scaleX(1);
 }
 </style>
